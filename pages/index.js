566769d1fe66b703d64bd47955269f1a36865039
Viral Patel <viral12111992@gmail.com>
2021-02-21 16:32:21 +0530

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm <strong>Viral</strong>. I'm a software enginer and a mentor. You can contact me on <a href="https://twitter.com/patelviral_12">Twitter.</a></p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
